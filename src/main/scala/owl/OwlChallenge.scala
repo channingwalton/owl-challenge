a4ad4955c82662a4f1188dc3df0040803e87e5b4
Channing Walton <channingwalton@mac.com>
2015-07-19 19:58:34 +0100

    refactoring to write the answers and the challenge

--- src/main/scala/owl/OwlChallenge.scala
+++ src/main/scala/owl/OwlChallenge.scala
@@ -10,13 +10,16 @@ object OwlChallenge extends Silver with Gold with App {
 
   val nColumns: Int = 2
   val colWidth: Int = 30
-
-  val writer = new PrintWriter("owl.tex", "UTF-8")
   private val equations: List[Equation] = goldTest.toList
 
-  writer.print(LatexRenderer(equations))
-  writer.flush()
-  writer.close()
+  write(equations, "owl")
 
-  "pdflatex owl.tex" #&& "open owl.pdf" !
+  def write(equations: List[Equation], filename: String) = {
+    val writer = new PrintWriter(s"$filename.tex", "UTF-8")
+    writer.print(LatexRenderer(equations))
+    writer.flush()
+    writer.close()
+
+    s"pdflatex $filename.tex" #&& s"open $filename.pdf" !
+  }
 }
